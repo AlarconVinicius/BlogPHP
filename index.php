2c0254876c36838c63262810266bd541bc661ec9
AlarconVinicius <alarcon.pqd74@gmail.com>
2022-08-21 17:47:57 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="icon" href="./img/ico.png">
+    <title>Blog</title>
+    <!-- https://www.lolpro.com.br/ -->
+</head>
+<body>
+    <div class="container">
+        <header class="header">
+            <nav class="navbar center">
+                <div class="nav-container width-limiter">
+                    <a href="#" class="link-img"><img src="./img/logo.png" alt="logo img"></a>
+                    <ul class="nav-items center">
+                        <li class="nav-item"><a href="#">Home</a></li>
+                        <li class="nav-item"><a href="#">Sobre</a></li>
+                        <li class="nav-item"><a href="#">Livros</a></li>
+                        <li class="nav-item"><a href="#">Coach</a></li>
+                        <li class="nav-item"><a href="#">Ajuda</a></li>
+                        <li class="nav-item"><a href="#">Contato</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <nav class="nav-category">
+                <div class="nav-container-category width-limiter">
+                    <ul class="nav-items-category center">
+                        <li class="nav-item-category"><a href="#">adc</a></li>
+                        <li class="nav-item-category"><a href="#">coach</a></li>
+                        <li class="nav-item-category"><a href="#">estratégia</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="search-container"> 
+                <form action="" class="search-form width-limiter">
+                    <div class="search-group">
+                        <input type="text" class="search-input" placeholder="O que você está procurando?">
+                        <input type="submit" class="search-btn" value="Pesquisar">
+                    </div>
+                </form>
+            </div>
+        </header>
+        
+        <div class="home center">
+            <div class="home-container width-limiter">
+                <div class="home-text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus modi alias consequuntur debitis a est, doloremque, esse nam eaque dolores deserunt. Sed dicta hic neque esse reprehenderit et error reprehenderit et error</p>
+                    <span>Author</span>
+                </div>
+                <div class="header-btn">
+                    BUTTON
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
